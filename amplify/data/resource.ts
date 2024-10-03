117aa1849be68aa35bdd7e075abd8d1b0b15a5cc
Payam <payam.mesgari@gmail.com>
2024-10-03 23:07:56 +0200

    first commit

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,12 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+  Recipe: a
+    .model({
+      title: a.string(),
+      description: a.string()
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,20 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [recipes, setRecipes] = useState<Array<Schema["Recipe"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.Recipe.observeQuery().subscribe({
+      next: (data) => setRecipes([...data.items]),
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>My recipes</h1>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.title}</li>
         ))}
       </ul>
       <div>
